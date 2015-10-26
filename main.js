c4ba102efc49453a7e4bb729dc746da76f07d48c
bbouley <b.bouley@hotmail.co.uk>
2015-10-26 15:33:36 -0600

    added dom binding function

--- main.js
+++ main.js
@@ -9,7 +9,46 @@ var classNamesArray = ['name'];
 //iterate through class name array
 //in that for loop grab dom elements associated with each class name
 //return an array of dom elements
+function getElements(array){
+    var elements = [];
+    for (var i = 0; i < array.length; i++) {
+        var elem = document.getElementsByClassName(array[i]);
+        elements.push(elem);
+    }
+    console.log(elements);
+    return elements;
+}
 
+//function calls
+
+var domElements = getElements(classNamesArray);
+
+//iterate through domelements
+//take each element and attach an event handler to it
+//onkeyup for event
+//perform some sort of action to bind the two inputs
+
+
+function domBinding(array) {
+    for (var i = 0; i < array.length; i++) {
+        var newArray = array[i];
+        for (var j = 0; j < newArray.length; j++) {
+            newArray[j].addEventListener('keyup', function() {
+                for (var k = 0; k < newArray.length; k++) {
+                    if(newArray[k] !== this.value) {
+                        newArray[k].value = this.value;
+                    }
+                }
+            });
+        }
+    }
+}
+
+function modelBinding(elements) {
+
+}
+
+domBinding(domElements);
 
 
 
