8be8c71f55fd2289a2bc3e1a4827ed2546318a51
bbouley <b.bouley@hotmail.co.uk>
2015-10-26 14:55:28 -0600

    added basic layout

---
+++ main.js
@@ -0,0 +1,15 @@
+// ** globals ** //
+
+var scope = {};
+//array of classes you want to link data-binding to
+var classNamesArray = ['name'];
+
+// ** functions ** //
+
+//iterate through class name array
+//in that for loop grab dom elements associated with each class name
+//return an array of dom elements
+
+
+
+
